8cf71073a649c4f64b9711d4a75c7b6a57ebc4b2
Mihail Dimitrov <36842438+bladedbicycle384@users.noreply.github.com>
2021-07-08 17:52:43 +0300

    Creating the main database
    It contains info about the listed jobs and admin users' credentials.

---
+++ database.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXITS JobsBoard;
+CREATE DATABASE JobsBoard;
+USE JobsBoard;
+
+CREATE TABLE Offers(
+  id int PRIMARY KEY NOT NULL auto_increment,
+  name varchar(150) NOT NULL,
+  description varchar(1000) DEFAULT NULL,
+  company varchar(100) NOT NULL,
+  salary int NOT NULL,
+);
+
+CREATE TABLE Admins(
+  id int PRIMARY KEY NOT NULL auto_increment,
+  username varchar(50) NOT NULL,
+  password varchar(64) NOT NULL
+);
